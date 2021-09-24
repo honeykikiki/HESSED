c4d620892d156db32fa5e04817c52a69ee267e02
honeykikiki <tjdwls75666@naver.com>
2021-09-24 22:31:36 +0900

    댓글작성, 더보기 완료

--- prepare/front/components/PostCard/PostComment.js
+++ prepare/front/components/PostCard/PostComment.js
@@ -1,22 +1,53 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import useInput from '../../hooks/useInput';
+import { ADD_COMMENT_REQUEST } from '../../reducers/post';
 
 import style from '../../styles/css/postComment.module.css';
 
-const PostContent = () => {
-  const commentPost = useCallback((e) => {
-    e.preventDefault();
-  }, []);
+const PostContent = ({ post }) => {
+  const dispatch = useDispatch();
+  const { addCommentDone } = useSelector((state) => state.post);
+
+  const id = useSelector((state) => state.user.me?.id);
+  const [comment, onChangeInput, setComment] = useInput('');
+
+  useEffect(() => {
+    if (addCommentDone) {
+      setComment('');
+    }
+  }, [addCommentDone]);
+
+  const commentPost = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!comment) {
+        return alert('댓글을 작성해주세요');
+      }
+      dispatch({
+        type: ADD_COMMENT_REQUEST,
+        data: {
+          content: comment,
+          postId: post.id,
+          User: { id, nickname: 'kikiki' },
+        },
+      });
+    },
+    [comment, id],
+  );
 
   return (
     <>
-      <form className={style.form} onClick={commentPost}>
+      <form className={style.form}>
         <textarea
           className={style.text}
           placeholder="댓글달기..."
           autoComplete="off"
           autoCorrect="off"
+          value={comment}
+          onChange={onChangeInput}
         ></textarea>
-        <button>게시</button>
+        <button onClick={commentPost}>게시</button>
       </form>
     </>
   );
--- prepare/front/components/PostCard/PostContent.js
+++ prepare/front/components/PostCard/PostContent.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
 
 import style from '../../styles/css/postContent.module.css';
+import useInput from '../../hooks/useInput';
 
-const PostContent = () => {
+const PostContent = ({ post }) => {
+  const [more, setMore] = useState(50);
+  const [moreButtonOnClick, setMoreButtonOnClick] = useState(true);
+
+  const contentMore = useCallback(() => {
+    setMore(post.content.length);
+    setMoreButtonOnClick((prev) => !prev);
+  }, [more, moreButtonOnClick]);
+  const contentbrief = useCallback(() => {
+    setMore(50);
+    setMoreButtonOnClick((prev) => !prev);
+  }, [more, moreButtonOnClick]);
+
   return (
     <article className={style.art}>
       <div>{`좋아요 0개`}</div>
-      <div>
-        <p>{`nickname #ooo #ooo ...입니다`}</p>
+      <div className={style.content}>
+        <p>{`${post.User.nickname}`}</p>
+        <span>{`${post.content.slice(0, more)} `}</span>
+        {moreButtonOnClick ? (
+          <button onClick={contentMore}>... 더보기</button>
+        ) : (
+          <button onClick={contentbrief}>간략하게보기</button>
+        )}
       </div>
+      {post.Comments.length > 2 && (
+        <p className={style.commentMore}>
+          {post.Comments.length}개의 댓글 모두보기
+        </p>
+      )}
       <div>
-        <p>{false ? `comment` : null}</p>
+        <p>
+          {post.Comments ? (
+            <div className={style.comment}>
+              <div>
+                <span>{`${post.Comments[0]?.User?.nickname}`}</span>
+                <span>{`${post.Comments[0].content}`}</span>
+              </div>
+              <div>
+                <span>{`${post.Comments[1]?.User?.nickname ?? ''}  `}</span>
+                <span>{`${post.Comments[1]?.content ?? ''} `}</span>
+              </div>
+            </div>
+          ) : null}
+        </p>
       </div>
-      <br />
       <div>
         <p>{`2020년 07월 08일`}</p>
       </div>
@@ -20,4 +57,16 @@ const PostContent = () => {
   );
 };
 
+PostContent.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.string,
+    user: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+    Likers: PropTypes.arrayOf(PropTypes.object),
+    Retweet: PropTypes.objectOf(PropTypes.any),
+  }).isRequired,
+};
 export default PostContent;
