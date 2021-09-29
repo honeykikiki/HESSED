1656d2b86f7f15abca65e90e24bbb521da82b7e5
honeykikiki <tjdwls75666@naver.com>
2021-09-29 19:21:10 +0900

    댓글의 답글 컴포넌트 분리, 답글보기 클릭시 다보이는 오류 해결

--- prepare/front/components/comment/Comment.js
+++ prepare/front/components/comment/Comment.js
@@ -1,40 +1,26 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 
 import style from '../../styles/css/dynamicComment.module.css';
-import useInput from '../../hooks/useInput';
-import Router from 'next/router';
+import CommentsToReply from './CommentsToReply';
+
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  COMMENT_TO_REPLY_CLOSE,
-  COMMENT_TO_REPLY_OPEN,
-} from '../../reducers/menu';
+import { COMMENT_TO_REPLY_OPEN } from '../../reducers/menu';
 import { ADD_COMMENT_REPLY_REQUEST } from '../../reducers/post';
 
 const Comment = ({ mainPosts, id }) => {
   const dispatch = useDispatch();
-  const { commentToReply } = useSelector((state) => state.menu);
   const { me } = useSelector((state) => state.user);
-  const { addCommentReplyDone } = useSelector((state) => state.post);
 
-  const [reply, onChangereply, setReply] = useInput(true);
   const [userId, setUserId] = useState(null);
   const [nickname, setNickname] = useState(null);
 
-  const [commentReply, onChangeInput, setCommentReply] = useInput('');
-
-  const ref = useRef();
-  const handleResizeHeight = useCallback(() => {
-    if (ref === null || ref.current === null) {
-      return;
+  useEffect(() => {
+    if (!me) {
+      Router.push('/');
     }
-    ref.current.style.height = '20px';
-    ref.current.style.height = ref.current.scrollHeight + 'px';
-  }, []); //댓글창 크기 자동조절
-
-  const onClickReply = useCallback(() => {
-    setReply((prev) => !prev);
-  }, [reply]);
+  }, [me]);
 
   const onClick = useCallback(
     (v) => () => {
@@ -45,46 +31,6 @@ const Comment = ({ mainPosts, id }) => {
       });
     },
     [userId, nickname],
-  );
-
-  const onClickReplyClose = useCallback(
-    (data) => {
-      dispatch({
-        type: COMMENT_TO_REPLY_CLOSE,
-        data,
-      });
-    },
-    [commentToReply],
-  );
-
-  useEffect(() => {
-    if (addCommentReplyDone) {
-      setCommentReply('');
-      ref.current.style.height = '20px';
-    }
-  }, [addCommentReplyDone]);
-
-  const onClickAddReply = useCallback(
-    (e) => {
-      e.preventDefault();
-      if (!commentReply) {
-        return alert('댓글을 작성해주세요');
-      }
-
-      dispatch({
-        type: ADD_COMMENT_REPLY_REQUEST,
-        data: {
-          content: commentReply,
-          postId: Number(id),
-          userId: userId,
-          User: {
-            id: me.id,
-            nickname: me.nickname,
-          },
-        },
-      });
-    },
-    [commentReply, userId],
   );
 
   return (
@@ -121,83 +67,34 @@ const Comment = ({ mainPosts, id }) => {
                 </button>
               </div>
             </div>
-            {/* {console.log(v)}
-            {console.log(Boolean(v.Comments[0]?.content))}
-            {console.log(v.Comments)} */}
+
             <li className={style.reply}>
-              {!v.Comments[i]?.content ? null : reply ? (
-                <button onClick={onClickReply}>
-                  <span></span>
-                  <p>답글 보기({v.Comments.length}개)</p>
-                </button>
-              ) : (
-                <button onClick={onClickReply}>
-                  <span></span>
-                  <p>답글 숨기기</p>
-                </button>
-              )}
-              {/* 댓글 더보기 컴포넌트 분리하기 */}
-              <div>
-                {!reply ? (
-                  <div>
-                    {v.Comments &&
-                      v.Comments.map((v, i) => {
-                        return (
-                          <ul>
-                            <li>
-                              <div>
-                                <div
-                                  className={style.userIcon}
-                                  // style={{
-                                  //   background: 'url(/icon/profle_img.png) ',
-                                  //   backgroundSize: 'contain',
-                                  // }}
-                                >
-                                  {v.User.nickname[0]}
-                                </div>
-                              </div>
-
-                              <div className={style.contentInComment}>
-                                <span>{v.User.nickname}</span>
-                                <span>{v.content}</span>
-                              </div>
-                            </li>
-                          </ul>
-                        );
-                      })}
-                  </div>
-                ) : null}
-
-                {!commentToReply ? (
-                  <div className={style.form}>
-                    <div className={style.commentBox}>
-                      <div onClick={onClickReplyClose}>X</div>
-                      <p>{nickname} 님에게 답글 남기는 중</p>
-                    </div>
-                    <form>
-                      <textarea
-                        className={style.text}
-                        ref={ref}
-                        onInput={handleResizeHeight}
-                        placeholder={`답글달기..`}
-                        autoComplete="off"
-                        autoCorrect="off"
-                        maxLength="140"
-                        value={commentReply}
-                        onChange={onChangeInput}
-                        required
-                      ></textarea>
-                      <button onClick={onClickAddReply}>게시</button>
-                    </form>
-                  </div>
-                ) : null}
-              </div>
+              <CommentsToReply
+                v={v}
+                i={i}
+                userId={userId}
+                nickname={nickname}
+                id={id}
+              />
             </li>
           </ul>
         );
       })}
     </div>
   );
+};
+
+Comment.propTypes = {
+  mainPosts: PropTypes.shape({
+    id: PropTypes.number,
+    user: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+    Likers: PropTypes.arrayOf(PropTypes.object),
+    Retweet: PropTypes.objectOf(PropTypes.any),
+  }).isRequired,
 };
 
 export default Comment;
---
+++ prepare/front/components/comment/CommentsToReply.js
@@ -0,0 +1,163 @@
+import React, { useRef, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import style from '../../styles/css/dynamicComment.module.css';
+import useInput from '../../hooks/useInput';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_COMMENT_REPLY_REQUEST } from '../../reducers/post';
+import { COMMENT_TO_REPLY_CLOSE } from '../../reducers/menu';
+
+const CommentsToReply = ({ v, i, userId, nickname, id }) => {
+  const dispatch = useDispatch();
+  const { commentToReply } = useSelector((state) => state.menu);
+  const { addCommentReplyDone } = useSelector((state) => state.post);
+  const { me } = useSelector((state) => state.user);
+
+  const [reply, onChangereply, setReply] = useInput(true);
+  const [commentReply, onChangeInput, setCommentReply] = useInput('');
+
+  const ref = useRef();
+  const handleResizeHeight = useCallback(() => {
+    if (ref === null || ref.current === null) {
+      return;
+    }
+    ref.current.style.height = '20px';
+    ref.current.style.height = ref.current.scrollHeight + 'px';
+  }, []); //댓글창 크기 자동조절
+
+  useEffect(() => {
+    if (ref === null || ref.current === null) {
+      return;
+    }
+    if (addCommentReplyDone) {
+      setCommentReply('');
+      ref.current.style.height = '20px';
+      dispatch({
+        type: COMMENT_TO_REPLY_CLOSE,
+      });
+    }
+  }, [addCommentReplyDone]);
+
+  const onClickReply = useCallback(() => {
+    setReply((prev) => !prev);
+  }, [reply]);
+
+  const onClickReplyClose = useCallback(() => {
+    dispatch({
+      type: COMMENT_TO_REPLY_CLOSE,
+    });
+  }, [commentToReply]);
+
+  const onClickAddReply = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!commentReply) {
+        return alert('댓글을 작성해주세요');
+      }
+
+      dispatch({
+        type: ADD_COMMENT_REPLY_REQUEST,
+        data: {
+          content: commentReply,
+          postId: Number(id),
+          userId: userId,
+          User: {
+            id: me.id,
+            nickname: me.nickname,
+          },
+        },
+      });
+    },
+    [commentReply, userId],
+  );
+
+  return (
+    <>
+      {!v.Comments[0] ? null : reply ? (
+        <button onClick={onClickReply}>
+          <span></span>
+          <p>답글 보기({v.Comments.length}개)</p>
+        </button>
+      ) : (
+        <button onClick={onClickReply}>
+          <span></span>
+          <p>답글 숨기기</p>
+        </button>
+      )}
+
+      {/* 댓글 더보기 컴포넌트 분리하기 */}
+      <div>
+        <div>
+          {!reply &&
+            v.Comments.map((v, i) => {
+              return (
+                <ul key={v.content}>
+                  <li>
+                    <div>
+                      <div
+                        className={style.userIcon}
+                        // style={{
+                        //   background: 'url(/icon/profle_img.png) ',
+                        //   backgroundSize: 'contain',
+                        // }}
+                      >
+                        {v.User.nickname[0]}
+                      </div>
+                    </div>
+
+                    <div className={style.contentInComment}>
+                      <span>{v.User.nickname}</span>
+                      <span>{v.content}</span>
+                    </div>
+                  </li>
+                </ul>
+              );
+            })}
+        </div>
+
+        {!commentToReply ? (
+          <div className={style.form}>
+            <div className={style.commentBox}>
+              <div onClick={onClickReplyClose}>X</div>
+              <p>{nickname} 님에게 답글 남기는 중</p>
+            </div>
+            <form>
+              <textarea
+                className={style.text}
+                ref={ref}
+                onInput={handleResizeHeight}
+                placeholder={`답글달기..`}
+                autoComplete="off"
+                autoCorrect="off"
+                maxLength="140"
+                value={commentReply}
+                onChange={onChangeInput}
+                required
+              ></textarea>
+              <button onClick={onClickAddReply}>게시</button>
+            </form>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+CommentsToReply.proptypes = {
+  v: PropTypes.shape({
+    id: PropTypes.number,
+    user: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+    Likers: PropTypes.arrayOf(PropTypes.object),
+    Retweet: PropTypes.objectOf(PropTypes.any),
+  }).isRequired,
+  userId: PropTypes.string.isRequired,
+  nickname: PropTypes.string.isRequired,
+  // id,
+};
+
+export default CommentsToReply;
