2f9a0b681f15a6d210058d733681467d1eb20984
honeykikiki <tjdwls75666@naver.com>
2021-12-14 21:38:54 +0900

    번들에널라이저 크로스엔브 환경변수설정 완료

--- prepare/front/pages/[id]/comment.js
+++ prepare/front/pages/[id]/comment.js
@@ -1,28 +1,18 @@
-//post/[id]/comment.js
 import React from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useDispatch, useSelector } from 'react-redux';
 
-import useInput from '../../hooks/useInput';
-
 import PostInComment from '../../components/comment/PostInComment';
-import MainLayout from '../../components/MainLayout';
 
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
-
-  // let windowScreenWidth = null;
-  // if (typeof window !== 'undefined') {
-  //   windowScreenWidth = window.screen.width;
-  // }
-
   const dispatch = useDispatch();
   const { mainPosts } = useSelector((state) => state.postMainAction);
   const post = mainPosts.find((v) => v.id === +id);
+
   return (
-    // <MainLayout>
     <>
       <Head>
         <title>{`HESSED ${post?.User.nickname} 님의 게사글`}</title>
@@ -41,10 +31,8 @@ const Post = () => {
         <meta property="og:image" content="/icon/HESSED_LOGO-W.png" />
         {/* <meta property="og:url" content={`https://nodebird.com/user/${id}`} /> */}
       </Head>
-
-      <PostInComment post={post} />
+      {post ? <PostInComment post={post} /> : null}
     </>
-    // </MainLayout>
   );
 };
 
