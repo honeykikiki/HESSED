4fbd0ac4818f7adbdd43796cd6fbbac94ba20f6a
honeykikiki <tjdwls75666@naver.com>
2021-12-15 09:41:07 +0900

    heroku 오류 해경중 url누락

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -2,7 +2,6 @@
 // export const baseURL = 'http://192.168.45.247:8081'; // 교회
 // export const baseURL = 'http://211.244.21.147:8081'; // 현준이형집
 export const baseURL = 'http://192.168.0.105:8081'; // 회사
-// export const baseURL = 'http://192.168.0.105:8081'; // 카페24
 // export const backUrl =
 //   process.env.NODE_ENV === 'production'
 //     ? 'http://api.nodebird.com'
