9d3a49050b4acb0aeae56ba86fb5a80d9211f512
honeykikiki <tjdwls75666@naver.com>
2021-12-15 17:28:34 +0900

    heroku next.config.js 변환

--- prepare/front/next.config.js
+++ prepare/front/next.config.js
@@ -1,18 +1,35 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
-module.exports = withBundleAnalyzer({
-  compress: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+// module.exports = withBundleAnalyzer({
+//   compress: true,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   future: {
+//     webpack5: true,
+//   },
+//   webpack(config) {
+//     const prod = process.env.NODE_ENV === 'production';
+//     return {
+//       ...config,
+//       mode: prod ? 'production' : 'development',
+//       devtool: prod ? 'hidden-source-map' : 'eval-source-map',
+//     };
+//   },
+// });
+
+const nextConfig = {
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /.*/,
+        contextRegExp: /__tests__/,
+      }),
+    );
+    return config;
   },
-  webpack(config) {
-    const prod = process.env.NODE_ENV === 'production';
-    return {
-      ...config,
-      mode: prod ? 'production' : 'development',
-      devtool: prod ? 'hidden-source-map' : 'eval-source-map',
-    };
-  },
-});
+};
+
+module.exports = nextConfig;
