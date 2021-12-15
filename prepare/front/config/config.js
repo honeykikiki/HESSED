3240274b6dec736049274c6bc841e2f323cc19f2
honeykikiki <tjdwls75666@naver.com>
2021-12-15 09:46:02 +0900

    heroku 확인

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,4 +1,3 @@
-// export const baseURL = 'http://211.244.21.147:8081';
 // export const baseURL = 'http://192.168.45.247:8081'; // 교회
 // export const baseURL = 'http://211.244.21.147:8081'; // 현준이형집
 export const baseURL = 'http://192.168.0.105:8081'; // 회사
