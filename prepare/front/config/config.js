d8742143982483c32df9cd1fe3604fe654fed4e9
honeykikiki <tjdwls75666@naver.com>
2021-12-19 23:22:01 +0900

    https통신 오류해결1

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 // export const baseURL = 'http://hessedweb.cafe24.com/'; // 카페24
-export const baseURL = 'http://hilltophessed.com/'; // 카페24
+export const baseURL = 'https://hilltophessed.com/'; // 카페24
 
 // export const baseURL =
 //   process.env.NODE_ENV === 'production'
