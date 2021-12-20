c6cea0fdb5e833940e1be7977f08ba218910bdec
honeykikiki <tjdwls75666@naver.com>
2021-12-20 14:02:37 +0900

    https적용

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 // export const baseURL = 'http://hessedweb.cafe24.com/'; // 카페24
-export const baseURL = 'http://hilltophessed.com/'; // 카페24
+// export const baseURL = 'http://hilltophessed.com/'; // 카페24
 
-// export const baseURL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://hilltophessed.com/'
-//     : 'http://hessedweb.cafe24.com/';
+export const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://hilltophessed.com/'
+    : 'http://hilltophessed.com/';
