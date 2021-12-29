f2234634e7d07e5afa26592c6ee261a1c875764c
honeykikiki <tjdwls75666@naver.com>
2021-12-29 18:16:01 +0900

    URL 재설정

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -2,10 +2,10 @@
 // export const baseURL = 'http://hessedweb.cafe24.com/'; // 카페24
 // export const baseURL = 'http://hilltophessed.com/'; // 카페24
 
-export const baseURL = 'http://192.168.0.105:8081'; // 회사
+// export const baseURL = 'http://192.168.0.105:8081'; // 회사
 // export const baseURL = 'http://192.168.45.247:8081'; //
 
-// export const baseURL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://hilltophessed.com/'
-//     : 'http://hilltophessed.com/';
+export const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://hilltophessed.com/'
+    : 'http://hilltophessed.com/';
